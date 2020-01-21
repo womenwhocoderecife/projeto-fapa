c28dfc01089e47e655adeae2f987109944988706
Ellen Cristina Xavier Coelho <ellen.coelho@pe.senai.br>
2020-01-21 12:25:48 -0300

    Adicionando view de acoes

---
+++ src/componentes/Acoes/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import "./Acoes.css";
+import list from "../../datas/acoes.json";
+function Acoes() {
+  const cards_line1 = [];
+  const cards_line2 = [];
+  list.map((item, index) => {
+    const image = require("../../assets/nossas-acoes/" + item.icon);
+    if (index <= 3) {
+      return cards_line1.push(
+        <div className="square">
+          <img className="image-square" src={image} alt="acoes"></img>
+          <span className="image-title"> {item.titulo} </span>{" "}
+        </div>
+      );
+    } else {
+      return cards_line2.push(
+        <div className="square">
+          <img className="image-square" src={image} alt="acoes"></img>
+          <span className="image-title"> {item.titulo} </span>{" "}
+        </div>
+      );
+    }
+  });
+
+  return (
+    <div>
+      <div className="cards-view"> {cards_line1} </div>
+      <div className="cards-view"> {cards_line2} </div>
+    </div>
+  );
+}
+
+export default Acoes;
