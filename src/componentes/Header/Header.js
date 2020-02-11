dddaec97b0dd5b26319d3ba27ec3ea12aa902a42
myllenaalves <myllenaalves971@gmail.com>
2020-02-11 11:30:52 -0300

    Adicionando modal do vídeo

--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -1,13 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import play from "../../assets/play_circle.svg";
 import YouTube from 'react-youtube';
-import video from "../../assets/institucional-video.mp4"
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import Modal from 'react-bootstrap/Modal'
 
 function Header() {
-
   function scrollPage() {
     window.scroll({
       top: 530,
@@ -15,17 +14,24 @@ function Header() {
       behavior: 'smooth'
     })
   }
-
+  const [videoShow, setVideoShow] = useState(false);
   return (
     <div className="header">
       <div className="video-fapa">
         <div className="video-fade"/>
         <img src={require('../../assets/gif.gif')} />
         <ExpandMoreIcon className="arrow" onClick={scrollPage}></ExpandMoreIcon>
-        <PlayCircleOutlineIcon className="play"></PlayCircleOutlineIcon>
+        <PlayCircleOutlineIcon className="play" onClick={() => setVideoShow(true)}></PlayCircleOutlineIcon>
       </div>
       <p className="faca-algo-por-alguem"> Faça Algo Por Alguém </p>
       <p className="ong-sem-fins-lucrati"> ONG sem fins lucrativos </p>
+      <Modal size="xl" show={videoShow} onHide={() => setVideoShow(false)}>
+        <Modal.Header closeButton>
+        </Modal.Header>
+        <Modal.Body>
+            <iframe width="1060" height="500" src="https://www.youtube.com/embed/wFvku4Dyp9g"></iframe>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
