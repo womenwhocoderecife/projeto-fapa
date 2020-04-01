3b52fee4c8d461048f91a80bf164a3e326aa902e
myllenaalves <myllenaalves971@gmail.com>
2020-04-01 16:32:54 -0300

    Adicionando copyright

--- src/componentes/Footer/Footer.js
+++ src/componentes/Footer/Footer.js
@@ -4,9 +4,14 @@ import { FaInstagram, FaPhone,FaMailBulk,FaAddressCard } from "react-icons/fa";
 function Footer() {
   return (
       <div className="container-footer">
-        <a className="item-footer" href='https://www.instagram.com/facaalgoporalguem/'> <FaInstagram className = "icon"/> facaalgoporalguem</a>
-        <a className="item-footer" href="mailto:facaalgoporalguem@gmail.com?subject=contatopelosite" target="_top"><FaMailBulk className = "icon" /> facaalgoporalguem@gmail.com</a>
-        <a className="item-footer"> <FaPhone className = "icon" /> (81) 9.9878.0508</a>
+        <div className="footer">
+          <a className="item-footer" href='https://www.instagram.com/facaalgoporalguem/'> <FaInstagram className = "icon"/> facaalgoporalguem</a>
+          <a className="item-footer" href="mailto:facaalgoporalguem@gmail.com?subject=contatopelosite" target="_top"><FaMailBulk className = "icon" /> facaalgoporalguem@gmail.com</a>
+          <a className="item-footer"> <FaPhone className = "icon" /> (81) 9.9878.0508</a>
+        </div>
+        <div className="copyright">
+          <a className="item-footer" href='https://womendevprojects.com.br'> Desenvolvido por &copy;Women Dev Projects.</a>
+        </div>
       </div>
   );
 }
