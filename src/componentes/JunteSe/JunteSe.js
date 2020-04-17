571ebb9dc9cb0ce487e0f48132e9fb848d57c26f
myllenaalves <myllenaalves971@gmail.com>
2020-04-17 12:25:00 -0300

    Ajustando problemas de responsividade

--- src/componentes/JunteSe/JunteSe.js
+++ src/componentes/JunteSe/JunteSe.js
@@ -4,21 +4,11 @@ import Button from "react-bootstrap/Button";
 function JunteSe() {
   return (
     <div className="JunteSe">
-      <div className="container-fluid p-0 ">
-        <div className="container">
-          <div className="row justify-content-center d-flex">
-            <h3 className="titulo">Você também pode fazer algo por alguém.</h3>
-          </div>
-          <div className="row justify-content-center d-flex ">
-            <Button
-              variant="outline-light"
-              className="junte-se-button"
-              href="/junte-se"
-            >
+        <div className="bg-laranja">
+          <h3 className="titulo">Você também pode fazer algo por alguém.</h3>
+          <Button variant="outline-light" className="junte-se-button" href="/junte-se">
               Junte-se
-            </Button>
-          </div>
-        </div>
+          </Button>
       </div>
     </div>
   );
